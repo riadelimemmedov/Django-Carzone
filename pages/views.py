952edb6fd1c14c69c2b4d3898e329dccd7c93b8d
Riad Elimemmedov <riad.elimemmedov@mail.ru>
2022-01-22 20:58:40 +0400

    updatednavbatcontents

--- pages/views.py
+++ pages/views.py
@@ -5,3 +5,15 @@ from django.shortcuts import render
 #!homeView
 def homeView(request):
     return render(request,'pages/home.html')
+
+#!aboutView
+def aboutView(request):
+    return render(request,'pages/about.html')
+
+#!servicesView
+def servicesView(request):
+    return render(request,'pages/services.html')
+
+#!contactView
+def contactView(request):#djangodaki view funksiyalari mutleq sekilde 1 ci parametr olarag request deyerini almalidir
+    return render(request,'pages/contact.html')
